ce0eb6aa5003acad0b2dfc298af801a089e353c3
Yassin HARKATI <yharkati@e3r13p11.42.fr>
2019-03-18 18:33:25 +0100

    RAS

--- client/src/routes/index/index.js
+++ client/src/routes/index/index.js
@@ -8,24 +8,25 @@ import '../../styles/home.css';
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          <div className="App__Aside">
-          </div>
+      <div className="App">
+         
+         <div className="App__Aside">
+         </div>
 
-          <div className="App__Form">
+         <div className="App__Form">
             <div className="PageSwitcher">
-                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Se connecter</NavLink>
-                <NavLink to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">S'inscrire</NavLink>
-              </div>
+                  <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Se connecter</NavLink>
+                  <NavLink to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">S'inscrire</NavLink>
+            </div>
 
-              <div className="FormTitle">
-                  <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Se connecter</NavLink> ou <NavLink to="/sign-up" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">S'inscrire</NavLink>
-              </div>
-      
-              <Route exact path="/" component={SignInForm} />
-              <Route path="/sign-up" component={SignUpForm}/>
-          </div>
-        </div>
+            <div className="FormTitle">
+                  <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Se connecter</NavLink> ou 
+                  <NavLink to="/sign-up" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">S'inscrire</NavLink>
+            </div>   
+            <Route exact path="/" component={SignInForm} />
+            <Route path="/sign-up" component={SignUpForm}/>
+         </div>
+      </div>
     );
   }
 }
