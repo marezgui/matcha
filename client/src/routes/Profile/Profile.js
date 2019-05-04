5da5016e2e5b39d316c8d46e0369acbef2c0b01c
yassin <yassinsisino2016@gmail.com>
2019-05-04 22:23:43 +0200

    petite modif dans la page profil

--- client/src/routes/Profile/Profile.js
+++ client/src/routes/Profile/Profile.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Layout from '../../hoc/Layout/Layout';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import { Col, Row, Container } from 'reactstrap';
 
 import './Profile.css';
 
@@ -28,13 +29,22 @@ class Profile extends Component {
             <div className="bloc">
                 <h3 className="box-title">Informations Personnel</h3>
                 <form className="box-content" onSubmit={this.editPerso}>
-                    <Input inputtype="input" label="First Name" type="text" name="firstName" placeholder="First Name" />
-                    <Input inputtype="input" label="Last Name" type="text" name="lastname" placeholder="Last Name" />
-                    <Input inputtype="input" label="Date of birth" type="date" name="dateOfBirth" placeholder="Date of birth" />
-                    <Input inputtype="input" label="Email" type="mail" name="email" placeholder="Email" />
-                    <Input inputtype="input" label="Login" type="text" name="login" placeholder="Login" />
-                    <Input inputtype="input" label="Password" type="password" name="password" placeholder="Password" />
-                    <Button >Modifier</Button>
+                    <Container>
+                        <Row>
+                            <Col><Input inputtype="input" label="First Name" type="text" name="firstName" placeholder="First Name" /></Col>
+                            <Col><Input inputtype="input" label="Last Name" type="text" name="lastname" placeholder="Last Name" /></Col>
+                            <Col><Input inputtype="input" label="Date of birth" type="date" name="dateOfBirth" placeholder="Date of birth" /></Col>
+                        </Row>
+                        <Row>
+                            <Col><Input inputtype="input" label="Email" type="mail" name="email" placeholder="Email" /></Col>
+                            <Col><Input inputtype="input" label="Login" type="text" name="login" placeholder="Login" /></Col>
+                        </Row>
+                        <Row>
+                            <Col><Input inputtype="input" label="Password" type="password" name="password" placeholder="Password" /></Col>
+                            <Col><Input inputtype="input" label="Confirme Password" type="password" name="password1" placeholder="Password" /></Col>
+                        </Row>
+                        <Button >Modifier</Button>
+                    </Container>
                 </form>
             </div>
         )
@@ -56,7 +66,7 @@ class Profile extends Component {
                         <Input inputtype="input" type="radio" label="Homme" name="genre" value="Homme" />
                         <Input inputtype="input" type="radio" label="Femme" name="genre" value="Femme" />
                     </fieldset>
-                    <Input label="Notification par mail" type="checkbox" name="notifications" value="1" checked />
+                    <Input label="Notification par mail" type="checkbox" name="notifications" />
                     <fieldset className="btn-radio">
                         <legend>Orientation:</legend>
                         <Input inputtype="input" type="radio" label="Hommes" name="orientation" value="Homme" />
@@ -69,8 +79,12 @@ class Profile extends Component {
         )
         return (
             <Layout>
-                {PersonelInfo}
-                {ProfilInfo}
+                <Container>
+                    <Row>
+                        <Col>{PersonelInfo}</Col>
+                        <Col>{ProfilInfo}</Col>
+                    </Row>
+                </Container>
             </Layout>
         )
     }
