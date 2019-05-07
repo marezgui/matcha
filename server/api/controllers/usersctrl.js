4874cac27e164f40761ad0405c4bbb6f96eb46e4
dovepa <you@example.com>
2019-05-07 21:47:10 +0200

    Modif / dove@frenchgorilla.com

--- server/api/controllers/usersctrl.js
+++ server/api/controllers/usersctrl.js
@@ -1,134 +1,114 @@
-import express from 'express'
-import bcrypt from 'bcrypt'
-import * as jwtUtils from '../utils/jwt.utils'
-import * as mod from '../models/usersmod'
-import util from 'util'
-import jwt from 'jsonwebtoken'
-//Passport pour le login/logout -> req.user
-import passport from 'passport'
-import * as op from '../utils/passport.utils'
+import express from 'express';
+import bcrypt from 'bcrypt';
+import util from 'util';
+import jwt from 'jsonwebtoken';
+// Passport pour le login/logout -> req.user
+import passport from 'passport';
+import * as mod from '../models/usersmod';
+import * as jwtUtils from '../utils/jwt.utils';
+import * as op from '../utils/passport.utils';
 
 // Constants
-const EMAIL_REGEX     = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const PASSWORD_REGEX  = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{6,}/;
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{6,}/;
 
 // FONCTIONS
 
 export const adduser = async (req, res) => {
-	const { email, login, password, firstName, lastName,
-		bio, genre, dateOfBirth, orientation } = req.body
-//	var location  = req.body.location;
-//	var photo = req.body.photo;
+  const { email, login, password, firstName, lastName,
+    bio, genre, dateOfBirth, orientation } = req.body;
+  //	var location  = req.body.location;
+  //	var photo = req.body.photo;
 
-	if (!(email || login || password || firstName || lastName
-		|| bio || genre || dateOfBirth || orientation))
-		return res.status(400).json({ 'error': 'Missing parameters.' });
+  if (!(email || login || password || firstName || lastName
+		|| bio || genre || dateOfBirth || orientation)) { return res.status(400).json({ error: 'Missing parameters.' }); }
 
-	if (login.length >= 15 || login.length <= 3)
-		return res.status(400).json({ 'error': 'Invalid login. (must be length 3 - 15)' });
+  if (login.length >= 15 || login.length <= 3) { return res.status(400).json({ error: 'Invalid login. (must be length 3 - 15)' }); }
 
-	if (!EMAIL_REGEX.test(email))
-		return res.status(400).json({ 'error': 'Invalid email.' });
+  if (!EMAIL_REGEX.test(email)) { return res.status(400).json({ error: 'Invalid email.' }); }
 
-	if (!PASSWORD_REGEX.test(password))
-		return res.status(400).json({ 'error': 'Invalid password. (must length > 5 and include 1 number & uppercase at least)' });
+  if (!PASSWORD_REGEX.test(password)) { return res.status(400).json({ error: 'Invalid password. (must length > 5 and include 1 number & uppercase at least)' }); }
 
-	const verifuserpseudo = util.promisify(mod.verifuserpseudo)
-	let resultuserpseudo = await verifuserpseudo(req)
-							.then(data => {return data})
-							.catch(err => { console.error(`[Error]: ${err}`)});
-	const verifuser = util.promisify(mod.verifuser)
-	const hash = util.promisify(bcrypt.hash)
-	let resultuser = await verifuser(req)
-							.then(data => {return data})
-							.catch(err => { console.error(`[Error]: ${err}`)});
-	if ((resultuser === 1) || (resultuserpseudo === 1))
-		return res.status(400).json({ "error" : "USER EXIST"})
-	else {
-		req.body.password = await hash(password, 5)
-									.then(data => {return data})
-									.catch(err => { console.error(`[Error]: ${err}`)});
-		return mod.adduser(req, res)
-	}
-}
+  const verifuserpseudo = util.promisify(mod.verifuserpseudo);
+  const resultuserpseudo = await verifuserpseudo(req)
+    .then(data => data)
+    .catch((err) => { console.error(`[Error]: ${err}`); });
+  const verifuser = util.promisify(mod.verifuser);
+  const hash = util.promisify(bcrypt.hash);
+  const resultuser = await verifuser(req)
+    .then(data => data)
+    .catch((err) => { console.error(`[Error]: ${err}`); });
+  if ((resultuser === 1) || (resultuserpseudo === 1)) { return res.status(400).json({ error: 'USER EXIST' }); }
+
+  req.body.password = await hash(password, 5)
+    .then(data => data)
+    .catch((err) => { console.error(`[Error]: ${err}`); });
+  return mod.adduser(req, res);
+
+};
 
 export const confirmmail = async (req, res) => {
-	const key = req.params.CONFIRMKEY
-	const checkkey = util.promisify(mod.checkkey)
-	let goodkey = await checkkey(key)
-							.then(data => {return data})
-							.catch(err => { console.error(`[Error]: ${err}`)});
-	if (goodkey === -1)
-		return res.status(201).json({ "message" : "BAD KEY"})
-	else {
-		mod.activeuser(goodkey, res)
-	}
-}
+  const key = req.params.CONFIRMKEY;
+  const checkkey = util.promisify(mod.checkkey);
+  const goodkey = await checkkey(key)
+    .then(data => data)
+    .catch((err) => { console.error(`[Error]: ${err}`); });
+  if (goodkey === -1) { return res.status(201).json({ message: 'BAD KEY' }); }
+
+  mod.activeuser(goodkey, res);
+
+};
 
 export const getallusers = (req, res) => {
-	mod.getallusers((err, success) => {
-		return res.status(200).json({success})
-	})
-}
+  mod.getallusers((err, success) => res.status(200).json({ success }));
+};
 
 export const getuser = (req, res) => {
-	const id = parseInt(req.params.id)
-	mod.getuser(id, (err, success) => {
-		return res.status(200).json({success})
-	})
-}
+  const id = parseInt(req.params.id);
+  mod.getuser(id, (err, success) => res.status(200).json({ success }));
+};
 
-export const getme = (req, res) =>{
-	return res.status(200).send(req.user)
-}
+export const getme = (req, res) => res.status(200).send(req.user);
 
 export const login = async (req, res) => {
-		if(req.body.email && req.body.password){
+  if (req.body.email && req.body.password) {
 		  var email = req.body.email;
-		}
-		else
-			return res.status(401).json({message:"Empty form"});
+  } else { return res.status(401).json({ message: 'Empty form' }); }
 
-		// usually this would be a database call:
-		const getuserbymail = util.promisify(mod.getuserbymail)
-		let user = await getuserbymail(email)
-									.then(data => {return data})
-									.catch(err => { console.error(`[Error]: ${err}`)})
+  // usually this would be a database call:
+  const getuserbymail = util.promisify(mod.getuserbymail);
+  const user = await getuserbymail(email)
+    .then(data => data)
+    .catch((err) => { console.error(`[Error]: ${err}`); });
 
-		const hashcmp = util.promisify(bcrypt.compare)
-		let passwdcmp = await hashcmp(req.body.password, user.password)
-									.then(data => {return data})
-									.catch(err => { console.error(`[Error]: ${err}`)});
-		console.log(passwdcmp)
-		if (user.activate === false)
-		{
-			return res.status(403).json({ message: `Your account is not activated yet. ${user.mail}` });
-		}
-		if(passwdcmp === true) {
+  const hashcmp = util.promisify(bcrypt.compare);
+  const passwdcmp = await hashcmp(req.body.password, user.password)
+    .then(data => data)
+    .catch((err) => { console.error(`[Error]: ${err}`); });
+  console.log(passwdcmp);
+  if (user.activate === false) {
+    return res.status(403).json({ message: `Your account is not activated yet. ${user.mail}` });
+  }
+  if (passwdcmp === true) {
 		  // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
-		  var payload = {id: user.id};
-		  var token = jwt.sign(payload, op.opts.secretOrKey );
-		  res.json({message: "ok", token: token});
-		} else {
-		  res.status(401).json({message:"passwords did not match."});
-		}
-	}
+		  const payload = { id: user.id };
+		  const token = jwt.sign(payload, op.opts.secretOrKey);
+		  res.json({ message: 'ok', token });
+  } else {
+		  res.status(401).json({ message: 'passwords did not match.' });
+  }
+};
 
-	//	Utile que si on utlise les cookies
+//	Utile que si on utlise les cookies
 export const logout = (req, res) => {
-	req.logout();
-	res.redirect('/');
-}
+  req.logout();
+  res.redirect('/');
+};
 
 export const deluser = (req, res) => {
-	mod.deluser(req.user.id, (err, success) => {
-		return res.status(200).json({success})
-	})
-}
+  mod.deluser(req.user.id, (err, success) => res.status(200).json({ success }));
+};
 
 export const edituser = (req, res) => {
-	mod.edituser(req, (err, success) => {
-		return res.status(200).json({success})
-	})
-}
-
+  mod.edituser(req, (err, success) => res.status(200).json({ success }));
+};
--- server/api/models/usersmod.js
+++ server/api/models/usersmod.js
@@ -1,157 +1,150 @@
-import { db } from './../../database'
-import uniqid from 'uniqid'
+import uniqid from 'uniqid';
+import { db } from '../../database';
 
 export const getallusers = (callback) => {
-	db.query('SELECT * FROM users ORDER BY id ASC', (err, res) => {
-		if (err.error)
-			callback(err.error, null)
-		callback(null, res)
-	})
-  }
+  db.query('SELECT * FROM users ORDER BY id ASC', (err, res) => {
+    if (err.error) { callback(err.error, null); }
+    callback(null, res);
+  });
+};
 
 export const verifuser = (request, callback) => {
-	const { email } = request.body
-	db.query('SELECT * FROM users WHERE mail = $1', [email], (err, res) => {
-		var data
-		if (err.error) {
-			callback(err, null)
-		}
-		if (res[0] === undefined){
-			data = 0
-		}
-		else{
-			data = 1
-		}
-		callback(null, data)
-	})
-}
+  const { email } = request.body;
+  db.query('SELECT * FROM users WHERE mail = $1', [email], (err, res) => {
+    let data;
+    if (err.error) {
+      callback(err, null);
+    }
+    if (res[0] === undefined) {
+      data = 0;
+    } else {
+      data = 1;
+    }
+    callback(null, data);
+  });
+};
 
 export const verifuserpseudo = (request, callback) => {
-	const { login } = request.body
-	db.query('SELECT * FROM users WHERE login = $1', [login], (err, res) => {
-		var data
-		if (err.error) {
-			callback(err, null)
-		}
-		if (res[0] === undefined){
-			data = 0
-		}
-		else{
-			data = 1
-		}
-		callback(null, data)
-	})
-}
+  const { login } = request.body;
+  db.query('SELECT * FROM users WHERE login = $1', [login], (err, res) => {
+    let data;
+    if (err.error) {
+      callback(err, null);
+    }
+    if (res[0] === undefined) {
+      data = 0;
+    } else {
+      data = 1;
+    }
+    callback(null, data);
+  });
+};
 
 export	const adduser = (request, response) => {
-		const { email, login, password, firstName, lastName,
-				bio, genre, dateOfBirth, orientation } = request.body
-		let confirmkey = uniqid("confirmmail-")
+  const { email, login, password, firstName, lastName,
+    bio, genre, dateOfBirth, orientation } = request.body;
+  const confirmkey = uniqid('confirmmail-');
 
-		db.query('INSERT INTO users (mail, login, password, firstName, lastName,\
+  db.query('INSERT INTO users (mail, login, password, firstName, lastName,\
 										bio, genre, dateOfBirth, orientation, confirmkey) \
-										VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)'
-					, [email, login, password, firstName, lastName,
-						bio, genre, dateOfBirth, orientation, confirmkey], (err, res) => {
-		if (err.error) {
-			console.log(err)
-		}
-		response.status(200).json({ "message" : `User add to database`,
-									"confirmkey" : confirmkey })
+										VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
+					 [email, login, password, firstName, lastName,
+    bio, genre, dateOfBirth, orientation, confirmkey], (err, res) => {
+    if (err.error) {
+      console.log(err);
+    }
+    response.status(200).json({ message: 'User add to database',
+      confirmkey });
 
-		})
-	}
+  });
+};
 
 
 export const checkkey = (key, callback) => {
-		db.query('SELECT * FROM users WHERE confirmkey = $1', [key], (err, res) => {
-		var data
-		if (err.error) {
-			callback(err, null)
-		}
-		if (res[0] === undefined){
-			data = -1
-		}
-		else{
-			data = res[0].id
-		}
-		callback(null, data)
-	})
-}
+  db.query('SELECT * FROM users WHERE confirmkey = $1', [key], (err, res) => {
+    let data;
+    if (err.error) {
+      callback(err, null);
+    }
+    if (res[0] === undefined) {
+      data = -1;
+    } else {
+      data = res[0].id;
+    }
+    callback(null, data);
+  });
+};
 
 export const activeuser = (id, response) => {
-	db.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
-	if (err.error) {
-		console.log(err)
-	}
-	if (res[0].activate === true)
-	{
-		response.status(200).json({ "message" : `User already activate`})
-	}
-	else{
-		db.query('UPDATE users SET activate=true WHERE id = $1', [id], (err, res) => {
-			if (err.error) {
-				console.log(err)
-			}
-			else {
-				response.status(200).json({ "message" : `User activate`})
-			}
-		})	}
-})
-}
+  db.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
+    if (err.error) {
+      console.log(err);
+    }
+    if (res[0].activate === true) {
+      response.status(200).json({ message: 'User already activate' });
+    } else {
+      db.query('UPDATE users SET activate=true WHERE id = $1', [id], (err, res) => {
+        if (err.error) {
+          console.log(err);
+        } else {
+          response.status(200).json({ message: 'User activate' });
+        }
+      });
+    }
+  });
+};
 
 export const getuser = (id, callback) => {
-	db.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
-	if (err.error) {
-		callback(err, null)
-	}
-	callback(null, res[0])
-})
-}
+  db.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
+    if (err.error) {
+      callback(err, null);
+    }
+    callback(null, res[0]);
+  });
+};
 
 export const getuserbymail = (email, callback) => {
-	db.query('SELECT * FROM users WHERE mail = $1', [email], (err, res) => {
-	if (err.error) {
-		callback(err, null)
-	}
-	callback(null, res[0])
-})
-}
+  db.query('SELECT * FROM users WHERE mail = $1', [email], (err, res) => {
+    if (err.error) {
+      callback(err, null);
+    }
+    callback(null, res[0]);
+  });
+};
 
 export const getuserbyid = (id, callback) => {
-	db.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
-	if (err.error) {
-		callback(err, null)
-	}
-	callback(null, res[0])
-})
-}
+  db.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
+    if (err.error) {
+      callback(err, null);
+    }
+    callback(null, res[0]);
+  });
+};
 
 export	const edituser = (request, callback) => {
-		const { email, login, password, firstName, lastName,
-		bio, genre, dateOfBirth, orientation } = request.body
-		const id = parseInt(request.params.id)
+  const { email, login, password, firstName, lastName,
+    bio, genre, dateOfBirth, orientation } = request.body;
+  const id = parseInt(request.params.id);
 
-		db.query('UPDATE users SET (mail, login, password, firstName, lastName,\
+  db.query('UPDATE users SET (mail, login, password, firstName, lastName,\
 			bio, genre, dateOfBirth, orientation, confirmkey) \
 			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
-		[email, login, password, firstName, lastName,
-			bio, genre, dateOfBirth, orientation, confirmkey],
+  [email, login, password, firstName, lastName,
+    bio, genre, dateOfBirth, orientation, confirmkey],
 
-		(err, res) => {
-			if (err.error) {
-				callback(err, null)
-			}
-			callback(null, "User modified")
-		}
-	)
-}
+  (err, res) => {
+    if (err.error) {
+      callback(err, null);
+    }
+    callback(null, 'User modified');
+  });
+};
 
 export	const deluser = (id, callback) => {
-		db.query('DELETE FROM users WHERE id = $1', [id], (err, res) => {
-		if (err.error) {
-			callback(err, null)
-		}
-		callback(null, "ok")
-		})
-	}
+  db.query('DELETE FROM users WHERE id = $1', [id], (err, res) => {
+    if (err.error) {
+      callback(err, null);
+    }
+    callback(null, 'ok');
+  });
+};
