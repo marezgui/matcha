ded49c1dd890d0c96c71e08f2eff3ba93d960b4c
dovepa <you@example.com>
2019-06-03 20:07:35 +0200

    adding url into mail

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,16 +1,85 @@
 import React, { Component } from 'react';
 import './Filter.css';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import TextField from '@material-ui/core/TextField';
 
 class Filter extends Component {
-    state = {
-
-    }
+    state = {}
 
     render() {
+
       return (
-        <section className="PeopleFilterBoxContent">
-          Filter / Sort
-        </section>
+        <div className="PeopleFilterBoxContent">
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell><h5>Search</h5></TableCell>
+                <TableCell align="right"><h6>Min</h6></TableCell>
+                <TableCell align="right"><h6>Max</h6></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h6>Distance</h6></TableCell>
+                <TableCell align="right">
+                  <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                  />
+                </TableCell>
+                <TableCell align="right">
+                  <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h6>Age</h6></TableCell>
+                <TableCell align="right">
+                  <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                  />
+                </TableCell>
+                <TableCell align="right">
+                  <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h6>Score</h6></TableCell>
+                <TableCell align="right">
+                  <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                  />
+                </TableCell>
+                <TableCell align="right">
+                  <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><h6>Tags</h6></TableCell>
+                <TableCell><h6>Tag select</h6></TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
       );
     }
 }
